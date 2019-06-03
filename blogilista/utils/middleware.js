6ea3af4934b2bd47a22be627a4e89a0fb1994ede
antjuntu <antjuntu@gmail.com>
2019-06-03 14:49:10 +0300

    4.19: blogilistan laajennus, step8

--- blogilista/utils/middleware.js
+++ blogilista/utils/middleware.js
@@ -19,6 +19,10 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
   }
 
   next(error)
