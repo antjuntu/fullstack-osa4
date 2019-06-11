fb8ad08c90c21d47e6f78b4d96234114619a1ac7
antjuntu <antjuntu@gmail.com>
2019-06-11 18:17:02 +0300

    Added populate to blogsRouter put

--- blogilista/controllers/blogs.js
+++ blogilista/controllers/blogs.js
@@ -81,7 +81,9 @@ blogsRouter.put('/:id', async (request, response, next) => {
     likes: body.likes || 0
   }
   try {
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    const updatedBlog = await Blog
+      .findByIdAndUpdate(request.params.id, blog, { new: true })
+      .populate('user', { username: 1, name: 1 })
     response.json(updatedBlog.toJSON())
   } catch(exception) {
     next(exception)
